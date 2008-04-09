d8a5796c821ba05f60c6250ff998184a290fa6a7
tuukka norri <tuukka.norri@karppinen.fi>
2008-04-09 18:32:32 +0000

    Moved files out of the Framework folder

---
+++ Sources/PGTSConnection.m
@@ -0,0 +1,166 @@
+//
+// PGTSConnection.m
+// BaseTen
+//
+// Copyright (C) 2008 Marko Karppinen & Co. LLC.
+//
+// Before using this software, please review the available licensing options
+// by visiting http://www.karppinen.fi/baseten/licensing/ or by contacting
+// us at sales@karppinen.fi. Without an additional license, this software
+// may be distributed only in compliance with the GNU General Public License.
+//
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License, version 2.0,
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// $Id$
+//
+
+@implementation PGTSConnection
+
+static void
+NoticeProcessor (void* connection, const char* message)
+{
+	//FIXME: handle the message.
+}
+
+static void
+SocketReady (CFSocketRef s, CFSocketCallBackType callbackType, CFDataRef address, const void* data, void* self)
+{
+	if (kCFSocketReadCallBack & callbackType)
+	{
+		[self readFromSocket];
+	}
+}
+
+- (id) init
+{
+	if ((self = [super init]))
+	{
+		mQueue = [[NSMutableArray alloc] init];
+	}
+	return self;
+}
+
+- (void) dealloc
+{
+	[mQueue release];
+	[super dealloc];
+}
+
+- (void) disconnect
+{
+	PQfinish (mConnection);
+}
+
+- (void) readFromSocket
+{
+	//When the socket is ready for read, send any available notifications and read results until 
+	//the socket blocks. If all results for the current query have been read, send the next query.
+	PQconsumeInput (mConnection);
+
+	PGnotify* pgNotification = NULL;
+	while ((pgNotification = PQnotifies (mConnection)))
+	{
+		NSDictionary* userInfo = [NSDictionary dictionaryWithObjectsAndKeys:
+			[NSNumber numberWithInt: pgNotification->be_pid],	kPGTSBackendPIDKey,
+			[NSNull null],										kPGTSNotificationExtraKey,
+			nil];
+		NSString* name = [NSString stringWithUTF8String: pgNotification->relname];
+		NSNotification* notification = [NSNotification notificationWithName: name object: self userInfo: userInfo];
+	    
+		[mNotificationCenter postNotification: notification];
+		PQfreeNotify (pgNotification);
+	}
+	
+	//FIXME: set the description.
+	PGTSQueryDescription* queryDescription = [mQueue firstObject];
+	while (! PQisBusy (mConnection))
+	{
+		PGresult* result = PQgetResult (mConnection);
+		if (! result)
+		{
+			[queryDescription connectionFinishedQuery: self];
+			break;
+		}
+		
+		if ([queryDescription sent])
+		{
+			//FIXME: process the result.
+			PGTSResultSet* resultSet = nil;
+			[queryDescription connection: self receivedResultSet: resultSet];
+		}
+	}
+	
+	if ([queryDescription finished])
+	{
+		[mQueue removeFirstObject];
+		[self sendNextQuery];
+	}
+}
+
+- (void) sendNextQuery
+{
+	PGTSQueryDescription* query = [mQueue firstObject];
+	log4AssertLog (! [query sent], @"Expected query not to have been sent. (%@)", query);
+	
+	//FIXME: send the query.
+	[query connectionSentQuery: self];
+}
+
+@end
+
+
+@implementation PGTSConnection (PGTSConnectorDelegate)
+
+- (void) connector: (PGTSConnector*) connector gotConnection: (PGConn *) connection succeeded: (BOOL) succeeded
+{
+	if (succeeded)
+	{
+		mConnection = connection;
+		
+		//Rather than call PQsendquery etc. multiple times, monitor the socket state.
+		PQsetnonblocking (connection, 0); 
+		//Use UTF-8.
+        PQsetClientEncoding (connection, "UNICODE"); 
+		PQexec (connection, "SET standard_conforming_strings TO true");
+		PQexec (connection, "SET datestyle TO 'ISO, YMD'");
+		PQsetNoticeProcessor (connection, &NoticeProcessor, (void *) self);
+        //FIXME: set other things as well?
+		
+		//Create a runloop source to receive data asynchronously.
+		CFSocketContext context = {0, self, NULL, NULL, NULL};
+		CFSocketCallBackTypes callbacks = kCFSocketReadCallBack | kCFSocketWriteCallBack;
+		mSocket = CFSocketCreateWithNative (NULL, PQsocket (mConnection), callbacks, &SocketReady, &context);
+		CFOptionFlags flags = ~kCFSocketCloseOnInvalidate & CFSocketGetSocketFlags (mSocket);
+		CFSocketSetSocketFlags (mSocket, flags);
+		mSocketSource = CFSocketCreateRunLoopSource (NULL, mSocket, 0);
+		
+		log4AssertLog (mSocket, @"Expected source to have been created.");
+		log4AssertLog (CFSocketIsValid (mSocket), @"Expected socket to be valid.");
+		log4AssertLog (mSocketSource, @"Expected socketSource to have been created.");
+		log4AssertLog (CFRunLoopSourceIsValid (mSocketSource), @"Expected socketSource to be valid.");
+		
+		CFRunLoopRef runloop = mRunLoop ?: CFRunLoopGetCurrent ();
+		CFStringRef mode = kCFRunLoopCommonModes;
+		CFRunLoopAddSource (runloop, mSocketSource, mode);
+		CFSocketDisableCallbackTypes (kCFSocketWriteCallBack);
+		CFSocketEnableCallbackTypes (kCFSocketReadCallBack);
+	}
+	else
+	{
+		//FIXME: handle the error.
+	}
+}
+
+@end
